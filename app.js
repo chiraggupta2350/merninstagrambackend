ba38f95809775182bd58990639652c7751b466de
chiraggupta <chiraggupta@topsinfosolutions.com>
2023-01-26 11:24:21 +0530

    fix cors

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
+const cors = require("cors")
 
 // Config
 require("dotenv").config();
 
-
+app.use(cors());
 app.use(express.json());
 
 
